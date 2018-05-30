9833c60d0563af9a5698f57daa113be3564790f7
MrSumit <35930711+MrSumit@users.noreply.github.com>
2018-05-30 23:51:01 +0530

    Add files via upload

--- Heart.java
+++ Heart.java
@@ -1,86 +1,133 @@
 import java.util.Random;
-
+import java.util.Scanner;
 class Heart { 
-  
-  protected static String ui = "";
-  protected static int ct = 0;
-  
-  protected static void add(int num) {
-    Random rd = new Random();
-    java.util.Scanner sc = new java.util.Scanner(System.in);
-    
-
-    int r1 = rd.nextInt();
-    int r2 = rd.nextInt();
-    int n1;
-    int n2;
-    do { n1 = rd.nextInt((int)Math.pow(10, num));
-      if (r1 < 0)
-      {
-        n1 *= -1;
-      }
       
-      n2 = rd.nextInt((int)Math.pow(10, num));
-      if (r2 < 0)
-      {
-        n2 *= -1;
-      }
-    } while (!(n1 >= (int)Math.pow(10, num - 1)) && !(n2 >= (int)Math.pow(10, num - 1)));
-    System.out.println("First number is " + n1);
-    System.out.println("Second number is " + n2);
-    System.out.print("Enter their sum = ");
-    String u = sc.next();
-    ui = u;
-    int s = n1 + n2;
-    try
-    {
-      if (s == Integer.valueOf(u).intValue())
-      {
-        System.out.println("Correct!!!");
-        ct += 1;
-      }
-      else {
-        System.out.println("Incorrect!!!");
-      }
-    } catch (NumberFormatException localNumberFormatException) {}
-  }
-  
-  protected static void sub(int num) { Random rd = new Random();
-    java.util.Scanner sc = new java.util.Scanner(System.in);
-    
-
-    int r1 = rd.nextInt();
-    int r2 = rd.nextInt();
-	int n1,n2;
-    do { n1 = rd.nextInt((int)Math.pow(10.0D, num));
-      if (r1 < 0)
-      {
-        n1 *= -1;
-      }
+      protected static String ui = "";
+      protected static int ct = 0;
       
-      n2 = rd.nextInt((int)Math.pow(10.0D, num));
-      if (r2 < 0)
-      {
-        n2 *= -1;
-      }
-    } while (!(n1 >= (int)Math.pow(10.0D, num - 1)) && !(n2 >= (int)Math.pow(10.0D, num - 1)));
-    System.out.println("First number is " + n1);
-    System.out.println("Second number is " + n2);
-    System.out.print("Enter their difference = ");
-    String u = sc.next();
-    ui = u;
-    int s = n1 - n2;
-    try
-    {
-      if (s == Integer.valueOf(u).intValue())
-      {
-        System.out.println("Correct!!!");
-        ct += 1;
-      }
-      else {
-        System.out.println("Incorrect!!!");
-      }
+    protected static void add(int num) {
+        Random rd = new Random();
+        Scanner sc = new Scanner(System.in);
+        int r1 = rd.nextInt();
+        int r2 = rd.nextInt();
+        int n1=0,c1=0;
+        int n2=0,c2=0;
+        do { 
+            if(c1==0)
+                n1 = rd.nextInt((int)Math.pow(10, num));
+            if(n1 > (int)Math.pow(10, num - 1))
+            {
+                if (r1 < 0)
+                {
+                    n1 *= -1;
+                }
+                c1=1;
+            }
+            if(c2==0)
+                n2 = rd.nextInt((int)Math.pow(10, num));
+            if(n2 > (int)Math.pow(10, num - 1))
+            {
+                if (r2 < 0)
+                {
+                    n2 *= -1;
+                }
+                c2=1;
+            }
+        } while (!(c1==1&&c2==1));
+        if(n1<0&&n2>=0)
+        {
+            System.out.println("First number is " + n1);
+            System.out.println("Second number is " + n2);
+        }
+        else if((n1<0&&n2<0)||(n1>-1&&n2>=0))
+        {
+            System.out.println("First number is  " + n1);
+            System.out.println("Second number is " + n2);
+        }
+        else if(n2<0&&n1>=0)
+        {
+            System.out.println("First number is   " + n1);
+            System.out.println("Second number is " + n2);
+        }
+        System.out.print("Enter their sum = ");
+        String u = sc.next();
+        ui = u;
+        int s = n1 + n2;
+        try
+        {
+            if (s == Integer.valueOf(u))
+            {
+                System.out.println("Correct!!!");
+                ct += 1;
+            }
+            else
+            {
+                System.out.println("Incorrect!!!");
+            }
+        } catch (NumberFormatException localNumberFormatException) {}
+        sc.close();
     }
-    catch (NumberFormatException localNumberFormatException) {}
-  }
+      
+    protected static void sub(int num) {
+        Random rd = new Random();
+        Scanner sc = new Scanner(System.in);
+        int r1 = rd.nextInt();
+        int r2 = rd.nextInt();
+        int n1=0,c1=0;
+        int n2=0,c2=0;
+        do { 
+            if(c1==0)
+                n1 = rd.nextInt((int)Math.pow(10, num));
+            if(n1 > (int)Math.pow(10, num - 1))
+            {
+                if (r1 < 0)
+                {
+                    n1 *= -1;
+                }
+                c1=1;
+            }
+            if(c2==0)
+                n2 = rd.nextInt((int)Math.pow(10, num));
+            if(n2 > (int)Math.pow(10, num - 1))
+            {
+                if (r2 < 0)
+                {
+                    n2 *= -1;
+                }
+                c2=1;
+            }
+        } while (!(c1==1&&c2==1));
+        if(n1<0&&n2>=0)
+        {
+            System.out.println("First number is " + n1);
+            System.out.println("Second number is " + n2);
+        }
+        else if((n1<0&&n2<0)||(n1>-1&&n2>=0))
+        {
+            System.out.println("First number is  " + n1);
+            System.out.println("Second number is " + n2);
+        }
+        else if(n2<0&&n1>=0)
+        {
+            System.out.println("First number is   " + n1);
+            System.out.println("Second number is " + n2);
+        }
+        System.out.print("Enter their difference = ");
+        String u = sc.next();
+        ui = u;
+        int s = n1 - n2;
+        try
+        {
+            if (s == Integer.valueOf(u))
+            {
+                System.out.println("Correct!!!");
+                ct += 1;
+            }
+            else
+            {
+                System.out.println("Incorrect!!!");
+            }
+        } catch (NumberFormatException localNumberFormatException) {}
+        sc.close();
+    }
 }
--- Mainu.java
+++ Mainu.java
@@ -1,27 +1,107 @@
+import java.awt.Robot;
 import java.util.Random;
 import java.util.Scanner;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
 class Mainu
 {
     protected static void outnBgd()
     {
+        byte cs = Byte.valueOf(insis("UserApp.csv"));
         Random r = new Random();
         Scanner s = new Scanner(System.in);
-        System.out.println("''WelCome To The Quiz Game''");
-        System.out.print("Enter the length of digit :");
-        int ln = s.nextInt(),ch,t=0;
-        do
+        if(cs==-1)
         {
-            ch=r.nextInt();
-            if(ch>0)
-                Heart.add(ln);
-            else if(ch<0)
-                Heart.sub(ln);
-            if(!(Heart.ui.equalsIgnoreCase("exit")))
-                t++;
-            System.out.println();
-        }while(!(Heart.ui.equalsIgnoreCase("exit")));
-        System.out.println("Your score is: "+Heart.ct+"/"+t);
-        System.out.println("''Thanks for playing my game!!!''");
-        System.out.println("''Have a good day!!!''");
+            String nst = null;
+            System.out.println("''WelCome To The Quiz Game''");
+            do
+            {
+                Scanner ns = new Scanner(System.in);
+                System.out.print("Are you using this program in Bluej? (Y/n):~");
+                nst = ns.next();
+                if(nst.equalsIgnoreCase("Y"))
+                {
+                    outsis("1","UserApp.csv");
+                    clearTML();
+                }
+                else if(nst.equalsIgnoreCase("n"))
+                    outsis("0","UserApp.csv");
+                ns.close();
+            }while(!(nst.equalsIgnoreCase("Y")||nst.equalsIgnoreCase("n")));
+        }
+        if(cs==-1)
+            cs = Byte.valueOf(insis("UserApp.csv"));
+        if(cs!=-1)
+            System.out.println("''WelCome To The Quiz Game''");
+        System.out.print("Enter the length of digit (1-10) :");
+        int ln = s.nextInt(),ch,t=0;
+        System.out.println();
+        if(ln<11&&ln>0)
+        {
+            do
+            {
+                ch=r.nextInt();
+                if(ch>0)
+                    Heart.add(ln);
+                else if(ch<0)
+                    Heart.sub(ln);
+                if(!(Heart.ui.equalsIgnoreCase("exit")))
+                    t++;
+                System.out.println();
+            }while(!(Heart.ui.equalsIgnoreCase("exit")));
+            System.out.println("Your score is: "+Heart.ct+"/"+t);
+            System.out.println("''Thanks for playing my game!!!''");
+            System.out.println("''Have a good day!!!''");
+        }
+        else
+        {
+            s.close();
+            Scanner sc = new Scanner(System.in);
+            System.out.print("''Invalid Input!!!''\nTry again (Y/n):~");
+            String op = sc.next();
+            if(op.equalsIgnoreCase("Y"))
+            {
+                if(cs==1)
+                    clearTML();
+                outnBgd();
+            }
+            if(op.equalsIgnoreCase("n"))
+                System.out.println("''Game End''");
+            sc.close();
+        }
+    }
+    private static void clearTML()
+    {
+        try
+        {
+            Robot r = new Robot();
+            r.keyPress(17);
+            r.keyPress((int)'K');
+            r.keyRelease(17);
+            r.keyRelease((int)'K');
+        }catch(Exception e){}
+    }
+    private static void outsis(String text, String fileName)
+    {
+        try {
+            PrintWriter out = new PrintWriter(fileName);
+            out.print(text);
+            out.close();
+        } catch (java.io.FileNotFoundException e) { 
+            System.out.println("Error: " + e);
+        }
+    }
+    private static String insis(String fileName) 
+    {
+        String pr=null;
+        try
+        {
+            BufferedReader in = new java.io.BufferedReader(new FileReader(fileName));
+            String str = null;
+            if((str = in.readLine()) != null)
+                pr=str;
+        } catch (java.io.IOException e) { System.out.println("Error: " + e); }
+        return pr;
     }
 }
